fe41e522bd74be0f150915a389bb2bd3f702e378
Ishnoor kaur <71192481+Ishnoor-kaur@users.noreply.github.com>
2023-10-19 13:07:13 +0530

    Time: 18 ms (32.01%), Space: 13.4 MB (27.59%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -13,19 +13,3 @@ class Solution(object):
         processed_t = process_string(t)
 
         return processed_s == processed_t
-solution = Solution()
-
-s1 = "ab#c"
-t1 = "ad#c"
-result1 = solution.backspaceCompare(s1, t1)
-print(result1)  # Output: True
-
-s2 = "ab##"
-t2 = "c#d#"
-result2 = solution.backspaceCompare(s2, t2)
-print(result2)  # Output: True
-
-s3 = "a#c"
-t3 = "b"
-result3 = solution.backspaceCompare(s3, t3)
-print(result3)  # Output: False
